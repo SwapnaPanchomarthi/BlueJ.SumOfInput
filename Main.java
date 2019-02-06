0bef66c208e586a29602050e2c6226932c67aea5
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-02-06 08:36:01 -0500

    Swapna-Sum in n input

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner; 
 public class Main {
 
     public static void main(String[] args){
+       
+       Scanner in = new Scanner(System.in);
+       System.out.println("Enter an integer");
+       int n;
+       n = in.nextInt();
+       
+       int sum = 0;
+       
+       for (int i=0; i<n; i++)
+       {
+           sum = sum+i;
 
     }
+    System.out.println("Sum is "+sum);
+}
 }
